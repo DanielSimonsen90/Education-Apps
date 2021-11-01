a354cf899b704ed0b237ec374c025abeaa36a043
DanielSimonsen90 <danielsimonsen90@gmail.com>
2021-11-01 11:27:39 +0100

    how to test

--- Apps/NoteKeeper/app/src/main/java/com/example/notekeeper/NoteListActivity.java
+++ Apps/NoteKeeper/app/src/main/java/com/example/notekeeper/NoteListActivity.java
@@ -61,5 +61,4 @@ public class NoteListActivity extends AppCompatActivity {
             }
         });
     }
-
 }
--- Apps/NoteKeeper/app/src/test/java/com/example/notekeeper/ExampleUnitTest.java
+++ Apps/NoteKeeper/app/src/test/java/com/example/notekeeper/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.example.notekeeper;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +15,80 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void createNote() {
+        //createNewNote
+        DataManager dm = DataManager.getInstance();
+        int newNoteIndex = dm.createNewNote(); //when using DataManager.createNewNote, it creates a new NoteInfo object with null values
+        NoteInfo note = dm.getNotes().get(newNoteIndex);
+
+        //note isn't null, but has null values
+        assertNotNull(note);
+        assertNull(note.getCourse());
+        assertNull(note.getTitle());
+        assertNull(note.getText());
+    }
+
+    @Test
+    public void createNoteAndRegret() {
+        createNote();
+        //go back
+    }
+
+    @Test
+    public void updateCourse() {
+        DataManager dm = DataManager.getInstance();
+        NoteInfo note = dm.getNotes().get(0);
+        CourseInfo currentCourse = note.getCourse();
+        CourseInfo course = dm.getCourses().get(0);
+        if (currentCourse == course) course = dm.getCourses().get(1);
+
+        note.setCourse(course);
+        assertNotEquals(course, currentCourse);
+        assertEquals(course, note.getCourse());
+    }
+
+    @Test
+    public void updateTitle() {
+        DataManager dm = DataManager.getInstance();
+        NoteInfo note = dm.getNotes().get(1);
+        String currentTitle = note.getTitle();
+        String newTitle = "Unit testing Title";
+
+        note.setTitle(newTitle);
+        assertNotEquals(currentTitle, note.getTitle());
+        assertEquals(newTitle, note.getTitle());
+    }
+
+    @Test
+    public void updateText() {
+        DataManager dm = DataManager.getInstance();
+        NoteInfo note = dm.getNotes().get(2);
+        String currentText = note.getText();
+        String newText = "Unit testing text";
+
+        note.setText(newText);
+        assertNotEquals(currentText, note.getText());
+        assertEquals(newText, note.getText());
+    }
+
+    @Test
+    public void updateTextAndCancel() {
+        updateText();
+        //cancel changes
+    }
+
+    @Test
+    public void deleteNote() {
+        DataManager dm = DataManager.getInstance();
+        List<NoteInfo> notes = dm.getNotes();
+        int index = 3;
+        NoteInfo note = notes.get(index);
+        dm.removeNote(index);
+        NoteInfo sameIndexNote = notes.get(index);
+
+        assertNotEquals(note, sameIndexNote);
     }
 }
